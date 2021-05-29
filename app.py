25006f3dff20dfa0b06200f2dcab28c6c73fd639
Kylie Burns <k_burns28@hotmail.com>
2021-05-29 12:03:44 +0800

    deployment workings

--- app.py
+++ app.py
@@ -34,48 +34,72 @@ Pet = create_classes(db)
 def home():
     return render_template("index.html")
 
+#Meat API page
+@app.route("/api/v1.0/meat")
+def meat():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-# Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
+    """Return a list of all water info"""
+    # Query all water data
+    results = session.query(meat.year, meat.beef, meat.lamb, meat.pork, meat.chicken).all()
 
-        pet = Pet(name=name, lat=lat, lon=lon)
-        db.session.add(pet)
-        db.session.commit()
-        return redirect("/", code=302)
+    session.close()
 
-    return render_template("form.html")
+    # Create a dictionary from the row data and append to a list of all_water
+    all_meat = []
+    for year, beef, lamb, pork, chicken in results:
+        meat_dict = {}
+        meat_dict["year"] = year
+        meat_dict["beef"] = beef
+        meat_dict["lamb"] = lamb
+        meat_dict["pork"] = pork
+        meat_dict["chicken"] = chicken
+        meat.append(water_dict)
+
+    return jsonify({"data":meat})
+
+# # Query the database and send the jsonified results
+# @app.route("/send", methods=["GET", "POST"])
+# def send():
+#     if request.method == "POST":
+#         name = request.form["petName"]
+#         lat = request.form["petLat"]
+#         lon = request.form["petLon"]
+
+#         pet = Pet(name=name, lat=lat, lon=lon)
+#         db.session.add(pet)
+#         db.session.commit()
+#         return redirect("/", code=302)
+
+#     return render_template("form.html")
 
 
-@app.route("/api/pals")
-def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+# @app.route("/api/pals")
+# def pals():
+#     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
 
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
+#     hover_text = [result[0] for result in results]
+#     lat = [result[1] for result in results]
+#     lon = [result[2] for result in results]
 
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 50,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
+#     pet_data = [{
+#         "type": "scattergeo",
+#         "locationmode": "USA-states",
+#         "lat": lat,
+#         "lon": lon,
+#         "text": hover_text,
+#         "hoverinfo": "text",
+#         "marker": {
+#             "size": 50,
+#             "line": {
+#                 "color": "rgb(8,8,8)",
+#                 "width": 1
+#             },
+#         }
+#     }]
 
-    return jsonify(pet_data)
+#     return jsonify(pet_data)
 
 
 if __name__ == "__main__":
